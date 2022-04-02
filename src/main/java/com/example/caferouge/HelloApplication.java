e178b630dfa3dea36c1bb095735cb799aff469c1
rewblx <bsong25@asu.edu>
2022-04-02 12:45:06 -0700

    login.fxml
    Boyang update, plz check login.fxml

--- src/main/java/com/example/caferouge/HelloApplication.java
+++ src/main/java/com/example/caferouge/HelloApplication.java
@@ -19,9 +19,33 @@ public class HelloApplication extends Application {
         stage.show();
         HelloController newProjectController = fxmlLoader.getController();
         newProjectController.setStage(stage);
+
+        fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+
     }
 
     public static void main(String[] args) {
+<<<<<<< Updated upstream
+=======
+        Dish d1=new Dish("Pizza",14,14);
+        GlobalData.dishes.add(d1);
+        d1 = new Dish("Handmade Noodles", 12, 12);
+        GlobalData.dishes.add(d1);
+        d1 = new Dish("Sushi", 12, 12);
+        GlobalData.dishes.add(d1);
+        d1 = new Dish("Garlic Butter Salmon", 12, 12);
+        GlobalData.dishes.add(d1);
+        GlobalData.dishesURL.put("Pizza",new Image("https://upload.wikimedia.org/wikipedia/commons/f/f1/Ruby_logo_64x64.png"));
+        GlobalData.dishesURL.put("Handmade Noodles",new Image("http://findicons.com/files/icons/832/social_and_web/64/apple.png"));
+        GlobalData.dishesURL.put("Sushi",new Image("http://antaki.ca/bloom/img/windows_64x64.png"));
+        GlobalData.dishesURL.put("Garlic Butter Salmon",new Image("http://files.softicons.com/download/social-media-icons/fresh-social-media-icons-by-creative-nerds/png/64x64/twitter-bird.png"));
+        GlobalData.dishesTemp=new ArrayList<>(GlobalData.dishes);
+
+
+
+
+
+>>>>>>> Stashed changes
         launch();
     }
 }
